c48cc44ea0d8b6018ee94f20ba98ee0ef421b9fa
Cecilia Ou <cou.code@gmail.com>
2022-09-27 15:09:27 -0700

    Added Image to signup form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, Length, ValidationError
+from wtforms.validators import DataRequired, Email, Length, ValidationError, URL
 from app.models import User
 
 
@@ -13,7 +13,7 @@ def user_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(message="Email is required"), user_exists])
+    email = StringField('email', validators=[DataRequired(message="Email is required"), Email(), user_exists])
     password = StringField('password', validators=[DataRequired()])
     firstName = StringField('First Name', 
                         validators=[DataRequired(message='First name is required'), 
@@ -21,3 +21,4 @@ class SignUpForm(FlaskForm):
     lastName = StringField('Last Name', 
                         validators=[DataRequired(message='Last name is required'), 
                         Length(max=100, message='Last name must be less than 100 characters')])
+    image = StringField('Image', validators=[URL(require_tld=True, message="Invalid Image Type")])
