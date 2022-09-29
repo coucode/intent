7d302ec25849bb2743c8fb783a72cb0057dd8130
Cecilia Ou <cou.code@gmail.com>
2022-09-28 21:46:24 -0700

    Created new Category List by User Component

--- react-app/src/components/LandingPage/NavBar.js
+++ react-app/src/components/LandingPage/NavBar.js
@@ -1,14 +1,33 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react'
 import LogoutButton from '../auth/LogoutButton'
 import './NavBar.css'
-import CategoryList from '../Categories/CategoryList'
 import { NavLink } from 'react-router-dom'
+import { useEffect } from 'react'
+import { getAllCategories } from '../../store/category'
+import CategoryListByUser from '../Categories/CategoryListByUser'
 
 function NavBar({ setShowNav }) {
+  const dispatch = useDispatch()
   const user = useSelector(state => state.session.user)
+  const [loaded, setLoaded] = useState(false)
+  const categoryObj = useSelector(state => state.category)
+  const categoryArr = Object.values(categoryObj)
 
+  useEffect(() => {
+    dispatch(getAllCategories)
+  }, [dispatch])
 
-  return (
+  useEffect(() => {
+    setLoaded(true)
+  }, [categoryObj])
+
+  let filtered;
+  if (categoryArr) {
+    filtered = categoryArr.filter(category => category.ownerId === user.id)
+  }
+
+  return loaded && filtered ? (
     <div className='navbar'>
       <div>
         <div className='navbar-top-container'>
@@ -16,7 +35,9 @@ function NavBar({ setShowNav }) {
           <div className='user-profile-container'>
             <img src={user.image} alt="user" className='user-image' />
             <div>
-              {user.firstName} {user.lastName}
+              <p className='navbar-profile-text'>
+                {user.firstName} {user.lastName}
+              </p>
             </div>
           </div>
           <div className='navbar-logout-container'>
@@ -31,11 +52,17 @@ function NavBar({ setShowNav }) {
         </div>
       </div>
       <div className='navbar-category-header-container'>
-        <p>My Categories</p>
+        <div className='navbar-category-header-inner-container'>
+          <p className='navbar-category-text'>My Categories</p>
+          <p className='navbar-category-text' id='navbar-count'>({filtered.length})</p>
+        </div>
       </div>
-
-      <CategoryList />
+      <div className='navbar-category-list-container'>
+        <CategoryListByUser categories={filtered} />
+      </div>
     </div>
+  ) : (
+    <h1>Loading...</h1>
   )
 }
 
