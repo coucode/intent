c31764864393d33924576156c1120be68c47c03b
Cecilia Ou <cou.code@gmail.com>
2022-10-02 17:29:44 -0700

    Main splashpage content complete

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,29 +1,71 @@
+import { useEffect, useState, useRef } from 'react'
 import { useSelector } from 'react-redux'
-import { Redirect } from 'react-router-dom'
-import LogoutButton from '../auth/LogoutButton'
+import SplashLogoutButton from '../auth/SplashLogoutButton'
 import LoginModal from '../LoginModal'
 import SignUpModal from '../SignUpModal'
 import './SplashPage.css'
 
 function SplashPage() {
-  // const user = useSelector(state => state.session.user)
+  const user = useSelector(state => state.session.user)
+  let count = useRef(0)
+  const [image, setImage] = useState('https://images.pexels.com/photos/4458554/pexels-photo-4458554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
 
-  // if (user){
-  //   return <Redirect to="/" />
-  // }
+  let sessionLinks = (
+    <div className='splash-start-container'>
+      <LoginModal />
+      <SignUpModal />
+    </div>
+  )
+
+  if (user) {
+    sessionLinks = (
+      <SplashLogoutButton />
+    )
+  }
+
+  useEffect(() => {
+    const images = [
+      "https://images.pexels.com/photos/4458554/pexels-photo-4458554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/5554289/pexels-photo-5554289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/4473905/pexels-photo-4473905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/5702419/pexels-photo-5702419.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/7972316/pexels-photo-7972316.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    ]
+    count.current++
+    let calc = count.current % 5
+    console.log("COUNT", count, "CALC", calc)
+    let interval = setInterval(() => { setImage(images[calc]) }, 3000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [image])
+  console.log("IMAGE", image)
 
   return (
-    <div>
-      <h1>This is the Splash Page!</h1>
-      <div>
-        <LoginModal />
+    <div className='splash-container'>
+      <nav className='splash-nav'>
+        <div className='splash-logo'>
+          <i className="fa-solid fa-meteor fa-2xl" id='splash-logo-color'></i>
+          <p>Intent</p>
+        </div>
+        {sessionLinks}
+      </nav>
+      <div className='splash-main-content'>
+        <div className='splash-quote-container'>
+          <h1 className='splash-quote'>Learn</h1>
+          <h1 className='splash-quote'>with Intent</h1>
+        </div>
+        <div
+          className='splash-images'
+          style={{ backgroundImage: `url(${image})` }}
+        >
+        </div>
       </div>
-      <div>
-        <SignUpModal />
+      <div className='splash-additional-content'>
+        <p>Hello</p>
       </div>
-      <div>
-        <LogoutButton />
-      </div>
+
+
     </div>
   )
 }
