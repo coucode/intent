c7543b77b1ca2a18efd34ecb29d41243aaf4e955
Cecilia Ou <cou.code@gmail.com>
2022-10-03 17:13:01 -0700

    Basic step carousel styling complete

--- react-app/src/components/Steps/StepCarousel.js
+++ react-app/src/components/Steps/StepCarousel.js
@@ -5,6 +5,9 @@ function StepCarousel({ steps }) {
   const [current, setCurrent] = useState(0)
   const [length, setLength] = useState(steps?.length)
   const [number, setNumber] = useState(1)
+  const [summaryStyle, setSummaryStyle] = useState('#555')
+  const [descStyle, setDescStyle] = useState('#555')
+
 
   useEffect(() => {
     setLength(steps.length)
@@ -24,12 +27,16 @@ function StepCarousel({ steps }) {
   const previous = () => {
     if (current > 0) {
       setCurrent(prevState => prevState - 1)
+      setDescStyle('#555')
+      setSummaryStyle('#555')
     }
   }
 
   const next = () => {
     if (current < (length - 1)) {
       setCurrent(prevState => prevState + 1)
+      setDescStyle('#555')
+      setSummaryStyle('#555')
     }
   }
 
@@ -43,14 +50,14 @@ function StepCarousel({ steps }) {
 
   return (
     <div className='sc-container'>
-      UNDER CONSTRUCTION
+      Click on the grey block to reveal the answer!
       <div className='sc-wrapper'>
-        <button
+        {current > 0 && <button
           className='sc-left-arrow'
           onClick={previous}
         >
           <i className="fa-solid fa-angle-left fa-xl"></i>
-        </button>
+        </button>}
         <div className='sc-content-container'>
           {
             sorted.map(step => {
@@ -64,9 +71,38 @@ function StepCarousel({ steps }) {
                   }}
                 >
                   <div className='sc-content'>
-                    <p>Number: {step.stepNumber}</p>
-                    <p>Summary: {step.summary} </p>
-                    <p>Description: {step.description}</p>
+                    <div className='sc-content-color-bar'></div>
+                    <div className='sc-content-inner-container'>
+                      <div className='sc-header-container'>
+                        <h2 className='sc-headers'>Step Number</h2>
+                      </div>
+                      <div className='sc-inner-text-container'>
+                        <p className='sc-inner-text'>{step.stepNumber}</p>
+                      </div>
+
+                      <div
+                        className='sc-header-container'>
+                        <h2 className='sc-headers'>Summary</h2>
+                      </div>
+                      <div
+                        className='sc-inner-text-container'
+                        style={{ backgroundColor: `${summaryStyle}` }}
+                        onClick={(e) => summaryStyle === '' ? setSummaryStyle("#555") : setSummaryStyle('')}
+                      >
+                        <p className='sc-inner-text'>{step.summary} </p>
+                      </div>
+
+                      <div className='sc-header-container'>
+                        <h2 className='sc-headers'>Description</h2>
+                      </div>
+                      <div className='sc-inner-text-container'
+                        id='sc-description'
+                        style={{ backgroundColor: `${descStyle}` }}
+                        onClick={(e) => descStyle === '' ? setDescStyle("#555") : setDescStyle('')}>
+                        <p className='sc-inner-text'>{(step.description) ? step.description : "No Description Provied"}</p>
+                      </div>
+
+                    </div>
                   </div>
                 </div>
               )
@@ -74,12 +110,12 @@ function StepCarousel({ steps }) {
           }
 
         </div>
-        <button
+        {current < (length - 1) && <button
           className='sc-right-arrow'
           onClick={next}
         >
           <i className="fa-solid fa-angle-right fa-xl"></i>
-        </button>
+        </button>}
       </div>
     </div>
   )
