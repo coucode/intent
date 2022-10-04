0663e0f9ef57d7de88e122365c78b1e4513678bd
Cecilia Ou <cou.code@gmail.com>
2022-10-04 14:12:47 -0700

    Added styling for active category navbar

--- react-app/src/components/Categories/CategoryListByUser.js
+++ react-app/src/components/Categories/CategoryListByUser.js
@@ -7,6 +7,7 @@ import './CategoryStyles/CategoryList.css'
 function CategoryListByUser({ categories }) {
   const dispatch = useDispatch()
   const [loaded, setLoaded] = useState(false)
+  const [activeNav, setActiveNav] = useState('')
 
 
   useEffect(() => {
@@ -17,13 +18,24 @@ function CategoryListByUser({ categories }) {
     setLoaded(true)
   }, [categories])
 
+  function isActive(link) {
+    if (activeNav.includes(link)) {
+      return 'active'
+    } else {
+      return 'inactive'
+    }
+  }
+
   return loaded && categories ? (
-    <div>
+    <div className='category-list-outer-container'>
       {categories.map(category => {
         return (
-          <div key={category.id} className='category-list-container'>
+          <div key={category.id}
+            className='category-list-container'
+            id={isActive(`${category.id}`)}
+            onClick={(e) => setActiveNav(`${category.id}`)}>
             <NavLink to={`/category/${category.id}/about`} className='category-list-navtext'>
-            <img src={category.icon} alt="category icon" className='category-list-icon' />
+              <img src={category.icon} alt="category icon" className='category-list-icon' />
               <p className='category-list-text'>
                 {category.name}
               </p>
