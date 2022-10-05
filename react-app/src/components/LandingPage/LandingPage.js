83187a545703544b0e012fa18ff8bd216d10125e
Cecilia Ou <cou.code@gmail.com>
2022-10-05 15:07:35 -0700

    Added developer links to navbar

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import NavBar from "./NavBar";
 import './LandingPage.css'
