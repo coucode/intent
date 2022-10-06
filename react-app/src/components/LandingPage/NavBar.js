29a83083847a2a9b584bb0577550a0344d0b1bb5
Cecilia Ou <cou.code@gmail.com>
2022-10-06 09:27:31 -0700

    Updated developer links so they open in a new tab

--- react-app/src/components/LandingPage/NavBar.js
+++ react-app/src/components/LandingPage/NavBar.js
@@ -84,10 +84,10 @@ function NavBar({ setShowNav }) {
           <p className='splash-dev-inner-text' style={{ color: "var(--orange-button-color)", fontSize: '30px' }}>Cecilia Ou</p>
         </div>
         <div className='splash-links'>
-          <a href="https://github.com/coucode">
+          <a href="https://github.com/coucode" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-square-github fa-2xl devLinks"></i>
           </a>
-          <a href="https://www.linkedin.com/in/ceciliasou">
+          <a href="https://www.linkedin.com/in/ceciliasou" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-linkedin fa-2xl devLinks"></i>
           </a>
         </div>
--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -84,11 +84,11 @@ function SplashPage() {
           <p className='splash-dev-inner-text' style={{color: "var(--orange-button-color)", fontSize: '30px'}}>Cecilia Ou</p>
 
         </div>
-        <div className='splash-links'>
-          <a href="https://github.com/coucode">
+        <div className='splash-links' >
+          <a href="https://github.com/coucode" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-square-github fa-2xl devLinks"></i>
           </a>
-          <a href="https://www.linkedin.com/in/ceciliasou">
+          <a href="https://www.linkedin.com/in/ceciliasou"  target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-linkedin fa-2xl devLinks"></i>
           </a>
         </div>
