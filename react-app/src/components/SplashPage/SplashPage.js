27d8e4e08633bba4646b6784a1a9fbca27082b89
Cecilia Ou <cou.code@gmail.com>
2022-11-08 19:26:59 -0800

    Updated splashpage with working carousel

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,4 +1,3 @@
-import { useEffect, useState, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory } from "react-router-dom"
 
@@ -11,9 +10,6 @@ import './SplashPage.css'
 function SplashPage() {
   const user = useSelector(state => state.session.user)
   const history = useHistory()
-
-  let count = useRef(0)
-  const [image, setImage] = useState('/static/images/splashpage/8.jpg')
 
   let sessionLinks = (
     <div className='splash-start-container'>
@@ -35,25 +31,6 @@ function SplashPage() {
     )
   }
 
-  useEffect(() => {
-    const images = [
-      "/static/images/splashpage/1.jpg",
-      "/static/images/splashpage/2.jpg",
-      "/static/images/splashpage/3.jpg",
-      "/static/images/splashpage/4.jpg",
-      "/static/images/splashpage/5.jpg",
-      "/static/images/splashpage/6.jpg",
-      "/static/images/splashpage/7.jpg",
-      "/static/images/splashpage/8.jpg",
-    ]
-    count.current++
-    let calc = count.current % 8
-    let interval = setInterval(() => { setImage(images[calc]) }, 5000)
-    return () => {
-      clearInterval(interval)
-    }
-  }, [image])
-
   return (
     <div className='splash-container'>
       {/* NAVIGATION BAR */}
@@ -71,25 +48,21 @@ function SplashPage() {
           <h1 className='splash-quote'>Learn</h1>
           <h1 className='splash-quote'>with Intent</h1>
         </div>
-        <div
-          className='splash-images'
-          style={{ backgroundImage: `url("${image}")` }}
-        >
-        </div>
+        <SplashCarousel />
       </div>
 
       {/* DEVELOPER INFORMATION */}
       <div className='splash-dev-info'>
         <div className='splash-dev-text'>
           <p className='splash-dev-inner-text'>Developer:</p>
-          <p className='splash-dev-inner-text' style={{color: "var(--orange-button-color)", fontSize: '30px'}}>Cecilia Ou</p>
+          <p className='splash-dev-inner-text' style={{ color: "var(--orange-button-color)", fontSize: '30px' }}>Cecilia Ou</p>
 
         </div>
         <div className='splash-links' >
           <a href="https://github.com/coucode" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-square-github fa-2xl devLinks"></i>
           </a>
-          <a href="https://www.linkedin.com/in/ceciliasou"  target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/ceciliasou" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-linkedin fa-2xl devLinks"></i>
           </a>
         </div>
@@ -144,7 +117,6 @@ function SplashPage() {
 
         </div>
       </div>
-      <SplashCarousel />      
 
     </div>
   )
