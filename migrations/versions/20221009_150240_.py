5e142f5fd6976f700fde41c31a6184355d9fb041
Cecilia Ou <cou.code@gmail.com>
2022-11-28 00:29:05 -0800

    Added Schema to migrations

--- migrations/versions/20221009_150240_.py
+++ migrations/versions/20221009_150240_.py
@@ -8,6 +8,10 @@ Create Date: 2022-10-09 15:02:40.193216
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = '8f32a65d8517'
@@ -67,6 +71,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
