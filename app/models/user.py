4515ea84523ba706836fdb0b06e9d38d96506b12
Cecilia Ou <cou.code@gmail.com>
2022-11-28 00:49:01 -0800

    Test

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
+        
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
